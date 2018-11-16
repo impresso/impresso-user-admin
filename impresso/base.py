e520a0fe2e8d0cfe196ec16e75981149749366f4
Daniele Guido <gui.daniele@gmail.com>
2018-11-16 08:58:06 +0100

    add dotenv
    Require dotenv library to be installed with:
    
    `pip install -U python-dotenv`

--- impresso/base.py
+++ impresso/base.py
@@ -1,8 +1,16 @@
 import os
 from django.core.exceptions import ImproperlyConfigured
+from dotenv import dotenv_values
+from pathlib import Path  # python3 only
 
+# e.g. set ENV=production to get .production.env file
+dotenv_filename = '.{0}.env'.format(os.environ.get('ENV', '')) if 'ENV' in os.environ else '.env'
+dotenv_path = Path('.') / dotenv_filename
+dotenv_dict = dotenv_values(dotenv_path=dotenv_path, verbose=True)
 
 def get_env_variable(var_name):
+    if var_name in dotenv_dict:
+        return dotenv_dict[var_name]
     try:
         return os.environ[var_name]
     except KeyError:
