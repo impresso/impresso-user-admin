766cff75783c763f4bed49b0c7e45c630b76d827
Daniele Guido <gui.daniele@gmail.com>
2018-11-18 14:49:32 +0100

    register models and modify admin

--- impresso/admin.py
+++ impresso/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import Newspaper, SearchQuery
+from .models import Issue, Newspaper, SearchQuery, ContentItem, Collection
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'year',)
+    search_fields = ['id']
 
 @admin.register(Newspaper)
 class NewspaperAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'start_year', 'end_year',)
+    search_fields = ['title']
 
 @admin.register(SearchQuery)
 class SearchQueryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'creator', 'name')
+    list_display = ('id', 'creator', 'name',)
+
+@admin.register(ContentItem)
+class ContentItemAdmin(admin.ModelAdmin):
+    search_fields = ['id']
+    autocomplete_fields = ('newspaper', 'issue',)
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'creator', 'name', 'status',)
+    autocomplete_fields = ('content_items',)
