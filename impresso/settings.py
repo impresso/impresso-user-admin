5338ba147c9e6d4ebb4f7912f96883a39eae025b
Daniele Guido <gui.daniele@gmail.com>
2019-01-23 15:50:28 +0100

    add static urls and subdomain fix

--- impresso/settings.py
+++ impresso/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_URL = get_env_variable('STATIC_URL', '/static/')
+STATIC_ROOT = get_env_variable('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
-STATIC_URL = '/static/'
-
+# if any
+FORCE_SCRIPT_NAME = get_env_variable('FORCE_SCRIPT_NAME', '/')
 
 # Celery
 CELERY_BROKER_URL = 'redis://localhost/4'
