f4b45826dc0ebb09b7893220ace431f784be64e5
Daniele Guido <gui.daniele@gmail.com>
2019-01-23 11:57:15 +0100

    add pymysql isntead of mysqldb

--- impresso/wsgi.py
+++ impresso/wsgi.py
@@ -6,8 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
 import os
+import pymysql
+
+# use pymysql isntead of MysQLdb
+pymysql.install_as_MySQLdb()
 
 from django.core.wsgi import get_wsgi_application
 
