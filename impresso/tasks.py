06fdc5793d3bd1c5b61bce2c2452eddb7e1adf84
Daniele Guido <gui.daniele@gmail.com>
2019-02-09 15:43:01 +0100

    add store_collectable_items tasks to add user collection to solr index

--- impresso/tasks.py
+++ impresso/tasks.py
@@ -17,49 +17,103 @@ def echo(self, message):
     print('Message: {0}'.format(message))
     return message
 
-# @app.task(bind=True)
-# def execute_solr_update(self, query, job_id, collection_id, skip=0, limit=500):
-#     # get the job so that we can update its status
-#     job = Job.objects.get(pk=job_id)
-#     items = CollectableItem.objects.filter(collection__id=collection_id)
-#     total = items.count()
-#     loops = math.ceil(total / limit)
-#
-#     # get the collectableItems ids in the collection
-#     items_ids = items.values('id')[skip:limit]
-#
-#     res = requests.get(settings.IMPRESSO_SOLR_URL_UPDATE, auth = settings.IMPRESSO_SOLR_AUTH_WRITE, params = {
-#         commit: 'true',
-#     })
-#
-#     meta = {
-#         'task': 'execute_solr_update',
-#         'progress': 0,
-#         'job_id': job.pk,
-#         'user_id': job.creator.pk,
-#         'extra': {
-#             'QTime': qtime,
-#             'total': total,
-#             # here we put the actual loops needed.
-#             'loops':  loops,
-#             'realloops': math.ceil(total / limit),
-#             'maxloops': settings.IMPRESSO_SOLR_EXEC_MAX_LOOPS,
-#             'page': page,
-#             'limit': limit,
-#             'skip': skip,
-#         },
-#         'warnings': [],
-#     }
-#     # should repeat until this gets None
-#     res.raise_for_status()
-#
-#     contents = res.json()
-#
-#
-#     pass
+
+@app.task(bind=True, autoretry_for=(Exception,), exponential_backoff=2, retry_kwargs={'max_retries': 5}, retry_jitter=True)
+def store_collectable_items(self, job_id, collection_id, skip=0, limit=10, taskname='store_collectable_items'):
+    # get the job so that we can update its status
+    job = Job.objects.get(pk=job_id)
+    job.status = Job.RUN
+
+    # get the collection!
+    collection = Collection.objects.get(pk=collection_id)
+
+    # get the collection status
+    items = CollectableItem.objects.filter(
+        collection = collection,
+        content_type = CollectableItem.ARTICLE
+    )
+    total = items.count()
+    loops = math.ceil(total / limit)
+    page = skip / limit + 1
+    progress = page / loops
+    taskstate = 'PROGRESS'
+
+    # get the collectableItems ids in the collection
+    items_ids = items.values_list('item_id', flat=True)[skip:limit]
+
+    # do the right thing
+    collection.add_items_to_index(items_ids=items_ids)
+
+    if page == loops:
+        taskstate = 'SUCCESS'
+        job.status = Job.DONE
+    else:
+        # queue job.
+        store_collectable_items.delay(
+            job_id = job.pk,
+            collection_id = collection.pk,
+            skip = int(page * limit),
+            taskname = taskname
+        )
+
+    # update status meta
+    meta = {
+        'task': taskname,
+        'progress': progress,
+        'job_id': job.pk,
+        'user_id': job.creator.pk,
+        'extra': {
+            # 'QTime': qtime,
+            'total': total,
+            # here we put the actual loops needed.
+            'loops':  loops,
+            'page': page,
+            'limit': limit,
+            'skip': skip,
+        },
+        'ids': [id for id in items_ids],
+    }
+
+    job.extra = json.dumps(meta)
+    job.save()
+
+    # update state
+    self.update_state(state = taskstate, meta = meta)
+
+    # return job, to be seen in info
+    return serializers.serialize('json', (job,))
+
+
 
 @app.task(bind=True)
-def count_items_in_collection(self, query, job_id, collection_id, content_type, skip=0):
+def store_collection(self, collection_id):
+    # get the collection
+    collection = Collection.objects.get(pk=collection_id)
+    # save current job!
+    job = Job.objects.create(
+        type=Job.SYNC_COLLECTION_TO_SOLR,
+        creator=collection.creator
+    );
+    # start update chain
+    store_collectable_items.delay(
+        job_id = job.pk,
+        collection_id = collection.pk,
+    )
+
+    # if alles gut
+    self.update_state(state = "INIT", meta = {
+        'task': 'sync_collection_in_solr',
+        'progress': 0,
+        'job_id': job.pk,
+        'user_id': collection.creator.pk,
+    })
+
+    return serializers.serialize('json', (job,))
+
+
+@app.task(bind=True)
+def count_items_in_collection(self, collection_id):
+
     pass
 
 @app.task(bind=True)
@@ -168,6 +222,30 @@ def execute_solr_query(self, query, job_id, collection_id, content_type, skip=0)
     #     job_id = job_id,
     #     user_id = job.creator.pk,
     # )
+
+@app.task(bind=True)
+def remove_collection(self, collection_id, user_id):
+    # check that the collection (still) exists!
+    collection = Collection.objects.get(pk=collection_id, creator__id=user_id);
+
+    # save current job!
+    job = Job.objects.create(
+        type=Job.DELETE_COLLECTION,
+        creator=collection.creator
+    );
+
+    # remove in bunch of 1000
+    remove_from_collection.delay(
+        job_id = job.pk,
+        collection_id = collection.pk,
+    )
+    return serializers.serialize('json', (job,))
+
+
+@app.task(bind=True)
+def remove_from_collection(self, job_id, collection_id, user_id):
+    pass
+
 
 @app.task(bind=True)
 def add_to_collection_from_query(self, collection_id, user_id, query, content_type):
