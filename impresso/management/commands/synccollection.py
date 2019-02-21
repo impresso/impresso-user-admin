41d2292cb488a1d661948b4f2fe942ccd9ca212a
Daniele Guido <gui.daniele@gmail.com>
2019-02-21 09:25:15 +0100

    simplify synccollection management command to use recently updated celery tasks

--- impresso/management/commands/synccollection.py
+++ impresso/management/commands/synccollection.py
@@ -1,7 +1,7 @@
 import requests, json
 from django.core.management.base import BaseCommand, CommandError
 from impresso.models import Collection, CollectableItem
-from impresso.tasks import store_collection
+from impresso.tasks import store_collection, count_items_in_collection
 from django.conf import settings
 
 
@@ -13,24 +13,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         collections = Collection.objects.filter(pk__in=options['collection_id'])
+        self.stdout.write('n. collection to sync: %s' % collections.count())
+
         for collection in collections:
             self.stdout.write('start syncing collection "%s"(pk=%s)...' % (collection.name, collection.pk))
 
-            items = CollectableItem.objects.filter(
-                collection = collection,
-                content_type = CollectableItem.ARTICLE
-            )
-            count = items.count()
-            items_ids = items.values_list('item_id', flat=True)
-
-            self.stdout.write('  items count: %s' % count)
-            self.stdout.write('  items sample: %s' % ','.join(items_ids[:10]))
-            # save collection count
-            collection.count_items = count
-            collection.save()
-
-            # collection.add_items_to_index(items_ids=items_ids[0:10])
-
+            try:
+                count_items_in_collection.delay(collection_id=collection.pk)
+            except Exception as e:
+                self.stderr.write(e)
             try:
                 store_collection.delay(collection_id=collection.pk)
             except Exception as e:
