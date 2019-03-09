0e51baaf6b8004a7d4dc08c7363a7afd98bab55a
Daniele Guido <gui.daniele@gmail.com>
2019-03-09 17:39:06 +0100

    add custom command to test a Job which run for 1 min

---
+++ impresso/management/commands/createtestjob.py
@@ -0,0 +1,27 @@
+import requests, json
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+
+from impresso.tasks import test
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Create a test job with fake progress for a given user'
+
+    def add_arguments(self, parser):
+        parser.add_argument('user_id', type=str)
+
+    def handle(self, user_id, *args, **options):
+        user = User.objects.get(pk=user_id)
+
+        self.stdout.write('\n\n--- start ---')
+        self.stdout.write('user id: "%s"' % user.pk)
+        self.stdout.write('user uid: "%s"' % user.profile.uid)
+
+        test.delay(
+            user_id = user.pk,
+        )
+        self.stdout.write('"test" task launched, check celery.')
+        self.stdout.write('---- end ----\n\n')
