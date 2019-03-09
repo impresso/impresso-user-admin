3675ee522f9838e031086d9ba21760c5d77a847b
Daniele Guido <gui.daniele@gmail.com>
2019-03-09 17:38:01 +0100

    add STOP status to interrup job task

--- impresso/models/job.py
+++ impresso/models/job.py
@@ -5,11 +5,17 @@ class Job(models.Model):
     BULK_COLLECTION_FROM_QUERY = 'BCQ'
     DELETE_COLLECTION = 'DCO'
     SYNC_COLLECTION_TO_SOLR = 'IDX'
+    EXPORT_COLLECTION_AS_CSV = 'EXP'
+    EXPORT_QUERY_AS_CSV = 'EXP'
+    TEST = 'TES'
 
     TYPE_CHOICES = (
         (BULK_COLLECTION_FROM_QUERY, 'Bulk collection from query'),
-        (SYNC_COLLECTION_TO_SOLR, 'Index collection in search engine'),
         (DELETE_COLLECTION, 'Delete collection'),
+        (SYNC_COLLECTION_TO_SOLR, 'Index collection in search engine'),
+        (EXPORT_COLLECTION_AS_CSV, 'Export collection as CSV'),
+        (EXPORT_QUERY_AS_CSV, 'Export query as CSV'),
+        (TEST, '10 minutes countdown, 1 percent every 6 seconds'),
     )
 
     READY = 'REA'
@@ -17,12 +23,14 @@ class Job(models.Model):
     DONE = 'DON'
     ERR = 'ERR'
     ARCHIVED = 'ARC'
+    STOP = 'STO'
 
     STATUS_CHOICES = (
         (READY, 'ready'),
         (RUN, 'running'),
         (DONE, 'Finished, no errors!'),
         (ARCHIVED, 'Finished, archived by the user'),
+        (STOP, 'Please stop'),
         (ERR, 'Ops, errors!'),
     )
 
@@ -35,6 +43,18 @@ class Job(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     extra = models.TextField()
+
+    def get_task_meta(self, taskname, progress=0.0, extra = {}):
+        meta = {
+            'task': taskname,
+            'progress': progress,
+            'job_id': self.pk,
+            'job_status': self.status,
+            'user_id': self.creator.pk,
+            'user_uid': self.creator.profile.uid,
+            'extra': extra
+        }
+        return meta
 
     class Meta:
         db_table = 'jobs'
