09cd4a5925f13531a4ec84c2864912d66bc2b469
Daniele Guido <gui.daniele@gmail.com>
2019-04-01 15:47:47 +0200

    add test query to command management

--- impresso/management/commands/exportqueryascsv.py
+++ impresso/management/commands/exportqueryascsv.py
@@ -5,7 +5,7 @@ from impresso.models import Job, Attachment
 
 from impresso.tasks import export_query_as_csv
 from django.conf import settings
-
+from impresso.solr import find_all, solr_doc_to_article
 
 class Command(BaseCommand):
     help = 'Create a test job with fake progress for a given user'
@@ -20,9 +20,14 @@ class Command(BaseCommand):
         self.stdout.write('\n\n--- start ---')
         self.stdout.write('user id: "%s"' % user.pk)
         self.stdout.write('user uid: "%s"' % user.profile.uid)
-        self.stdout.write('query: (%s)' % q)
+        self.stdout.write('query q: (%s)' % q)
+        self.stdout.write('query fl: (%s)' % settings.IMPRESSO_SOLR_FIELDS)
 
-        export_query_as_csv.delay(query=q, user_id=user_id)
-
-        self.stdout.write('"test" task launched, check celery.')
+        # test query
+        results = find_all(q=q, fl=settings.IMPRESSO_SOLR_FIELDS)
+        self.stdout.write('found docs: (%s)' % results['response']['numFound'])
+        if results['response']['numFound'] > 0:
+            self.stdout.write('row example: (%s)' % solr_doc_to_article(results['response']['docs'][0]))
+            export_query_as_csv.delay(query=q, user_id=user_id)
+            self.stdout.write('"test" task launched, check celery.')
         self.stdout.write('---- end ----\n\n')
