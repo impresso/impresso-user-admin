9aaf5b388a4aa3d54545eb6cdfb67ef3576eddae
Daniele Guido <gui.daniele@gmail.com>
2019-10-30 12:34:31 +0100

    update settings

--- impresso/settings.py
+++ impresso/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xvqp74e(#pd**ubd*3yv$+kf$li8*ml+!r_=_&$5vbu-1yww$g'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_env_variable('DEBUG') == 'True'
