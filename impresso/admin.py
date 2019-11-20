5eb8dad3fefe6fbd28573fee4e108f602db00f5f
Daniele Guido <gui.daniele@gmail.com>
2019-11-20 14:02:53 +0100

    add date of creation to the admin panel

--- impresso/admin.py
+++ impresso/admin.py
@@ -35,7 +35,8 @@ class ContentItemAdmin(admin.ModelAdmin):
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
     search_fields = ['name', 'creator', 'id']
-    list_display = ('id', 'creator', 'name', 'status',)
+    list_display = ('id', 'creator', 'name', 'status', 'date_created', )
+    readonly_fields = ('date_created', 'date_last_modified', )
 
 @admin.register(CollectableItem)
 class CollectableItemAdmin(admin.ModelAdmin):
