97ed43183bd212c3161b4da885bf424795069901
Daniele Guido <gui.daniele@gmail.com>
2020-02-12 13:42:11 +0100

    refactor store_collection, use common utils

--- impresso/tasks.py
+++ impresso/tasks.py
@@ -273,14 +273,17 @@ def export_query_as_csv(self, user_id, query, description='', query_hash='', sea
 
 
 
-@app.task(bind=True, autoretry_for=(Exception,), exponential_backoff=2, retry_kwargs={'max_retries': 5}, retry_jitter=True)
+@app.task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={'max_retries': 5}, retry_jitter=True)
 def store_collectable_items(self, job_id, collection_id, skip=0, limit=50, taskname='store_collectable_items', method='add_to_index', items_ids=None):
     # get the job so that we can update its status
     job = Job.objects.get(pk=job_id)
 
     # get the collection!
     collection = Collection.objects.get(pk=collection_id)
-
+    extra = {
+        'collection_id': collection_id,
+        'collection': get_collection_as_obj(collection),
+    }
     # get the collection status
     items = CollectableItem.objects.filter(
         collection = collection,
@@ -301,87 +304,69 @@ def store_collectable_items(self, job_id, collection_id, skip=0, limit=50, taskn
         method,
     ))
 
+    # generate extra from job stats
+    page, loops, progress = get_job_stats(job=job, skip=skip, limit=limit, total=total)
+
+    if is_task_stopped(task=self, job=job, progress=progress, extra=extra):
+        logger.info('[job:{}] Task STOPPED. Bye!'.format(job.pk))
+        return
+
     if total == 0:
         logger.info('Collection(pk:{}) nothing to do!'.format(collection.pk))
-        # nothing to do! return an error
-        progress = 0
-        loops = 0
-        page = -1
-        items_ids = []
-    else:
-        loops = math.ceil(total / limit)
-        page = skip / limit + 1
-        progress = page / loops
-        # get the collectableItems ids in the collection
-        items_ids = items.values_list('item_id', flat=True)[0:limit]
-        logger.info('Collection(pk:{}) store {} items_ids, first three ids: {}'.format(
+        update_job_completed(task=self, job=job, extra=extra)
+        return
+    # update status to RUN
+    job.status = Job.RUN
+
+    # update pregress accordingly
+    update_job_progress(task=self, job=job, progress=progress)
+
+    # get the collectableItems ids in the collection
+    items_ids = items.values_list('item_id', flat=True)[0:limit]
+    logger.info('Collection(pk:{}) store {} items_ids, first three ids: {}'.format(
+        collection.pk,
+        len(items_ids),
+        items_ids[:3],
+    ))
+
+    # add items to the index
+    if method == 'add_to_index':
+        result = collection.add_items_to_index(items_ids=items_ids, logger=logger)
+        logger.info('Collection(pk:{}) n.items to update in db with (indexed=True): {}, first three ids: {}, added to solr: {}'.format(
             collection.pk,
-            len(items_ids),
-            items_ids[:3],
+            len(result.get('docs')),
+            result.get('docs')[:3],
+            len(result.get('todos')),
         ))
+        items_ids_to_add = [doc.get('id') for doc in result.get('docs')]
+        # perform query
+        indexed = CollectableItem.objects.filter(
+            collection = collection,
+            item_id__in=items_ids_to_add
+        ).update(indexed=True)
 
-        # add items to the index
-        if method == 'add_to_index':
-            result = collection.add_items_to_index(items_ids=items_ids, logger=logger)
-            logger.info('Collection(pk:{}) n.items to update in db with (indexed=True): {}, first three ids: {}, added to solr: {}'.format(
-                collection.pk,
-                len(result.get('docs')),
-                result.get('docs')[:3],
-                len(result.get('todos')),
-            ))
-            items_ids_to_add = [doc.get('id') for doc in result.get('docs')]
-            # perform query
-            indexed = CollectableItem.objects.filter(
-                collection = collection,
-                item_id__in=items_ids_to_add
-            ).update(indexed=True)
-
-            logger.info('Collection(pk:{}) success, {} items updated in db.'.format(
-                collection.pk,
-                indexed,
-            ))
-        else:
-            collection.remove_items_to_index(items_ids=items_ids)
-
-    if page == loops:
-        taskstate = 'SUCCESS'
-        job.status = Job.DONE
-    elif page == -1:
-        taskstate = 'ERROR'
-        job.status = Job.ERR
+        logger.info('Collection(pk:{}) success, {} items updated in db.'.format(
+            collection.pk,
+            indexed,
+        ))
     else:
-        taskstate = 'PROGRESS'
-        job.status = Job.RUN
-        # queue job.
+        collection.remove_items_to_index(items_ids=items_ids)
+
+    if page < loops:
         store_collectable_items.delay(
             job_id = job.pk,
             collection_id = collection.pk,
             skip = int(page * limit),
-            taskname = taskname
+            taskname = taskname,
+            items_ids=items_ids,
         )
-
-    meta = job.get_task_meta(taskname=taskname, progress=progress, extra = {
-        'total': total,
-        'skip': skip,
-        'limit': limit,
-        'page': page,
-        'loops': loops,
-        'collection_id': collection.pk,
-        'ids': [id for id in items_ids],
-    })
-    job.extra = json.dumps(meta)
-    job.save()
-
-    # update state
-    self.update_state(state = taskstate, meta = meta)
-
-    # return job, to be seen in info
-    return serializers.serialize('json', (job,))
+    else:
+        update_job_completed(task=self, job=job, extra=extra)
 
 
 
 @app.task(bind=True)
-def store_collection(self, collection_id):
+def store_collection(self, collection_id, items_ids=None):
     # get the collection
     collection = Collection.objects.get(pk=collection_id)
     # save current job!
@@ -389,26 +374,21 @@ def store_collection(self, collection_id):
         type=Job.SYNC_COLLECTION_TO_SOLR,
         creator=collection.creator
     );
+
+    update_job_progress(task=self, job=job, taskstate=TASKSTATE_INIT, progress=0.0, extra={
+        'collection': get_collection_as_obj(collection),
+    })
+
+    logger.info('Collection(pk:{}) delay store_collectable_items, with items: {}'.format(
+        collection.pk,
+        items_ids,
+    ))
     # start update chain
     store_collectable_items.delay(
         job_id = job.pk,
         collection_id = collection.pk,
+        items_ids=items_ids,
     )
-    meta = {
-        'task': 'sync_collection_in_solr',
-        'progress': 0,
-        'job_id': job.pk,
-        'job_status': job.status,
-        'user_id': collection.creator.pk,
-        'user_uid': collection.creator.profile.uid,
-    }
-
-    job.extra = json.dumps(meta)
-    job.save()
-    # if alles gut
-    self.update_state(state = "INIT", meta = meta)
-
-    return serializers.serialize('json', (job,))
 
 
 @app.task(bind=True)
