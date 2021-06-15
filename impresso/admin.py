6c737b578fb7f3819d3e2548a80bc34abb3c25ef
Daniele Guido <gui.daniele@gmail.com>
2021-06-15 16:15:10 +0200

    add relevant info in admin table

--- impresso/admin.py
+++ impresso/admin.py
@@ -1,36 +1,41 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from django.urls import reverse
-from django.utils.html import escape
 
-from .models import Profile, Issue, Job, Page, Newspaper, SearchQuery, ContentItem
+from .models import Profile, Issue, Job, Page, Newspaper
+from .models import SearchQuery, ContentItem
 from .models import Collection, CollectableItem, Tag, TaggableItem
 from .models import Attachment, UploadedImage
+
 
 @admin.register(Issue)
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('id', 'year', 'newspaper',)
     search_fields = ['id']
 
+
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     list_display = ('id', 'newspaper', 'ocr_quality',)
     search_fields = ['id']
+
 
 @admin.register(Newspaper)
 class NewspaperAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'start_year', 'end_year',)
     search_fields = ['title']
 
+
 @admin.register(SearchQuery)
 class SearchQueryAdmin(admin.ModelAdmin):
     list_display = ('id', 'creator', 'description', 'date_created',)
+
 
 @admin.register(ContentItem)
 class ContentItemAdmin(admin.ModelAdmin):
     search_fields = ['id']
     autocomplete_fields = ('newspaper', 'issue',)
+
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
@@ -38,20 +43,24 @@ class CollectionAdmin(admin.ModelAdmin):
     list_display = ('id', 'creator', 'name', 'status', 'date_created', )
     readonly_fields = ('date_created', 'date_last_modified', )
 
+
 @admin.register(CollectableItem)
 class CollectableItemAdmin(admin.ModelAdmin):
     list_display = ('item_id', 'collection', 'content_type', 'date_added',)
     autocomplete_fields = ('collection',)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('id', 'creator', 'name',)
 
+
 @admin.register(TaggableItem)
 class TaggableItemAdmin(admin.ModelAdmin):
     list_display = ('item_id', 'tag', 'content_type', 'date_added',)
     autocomplete_fields = ('tag',)
+
 
 @admin.register(UploadedImage)
 class UploadImageAdmin(admin.ModelAdmin):
@@ -69,10 +78,15 @@ class AttachmentInline(admin.StackedInline):
     #     else:
     #         return []
 
+
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     inlines = (AttachmentInline,)
-    list_display = ('id', 'creator', 'type', 'description', 'date_created', 'status', 'attachment')
+    list_display = (
+        'id', 'creator', 'type', 'description', 'date_created',
+        'status', 'attachment'
+    )
+
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -81,18 +95,19 @@ class ProfileInline(admin.StackedInline):
 
     def get_readonly_fields(self, request, user=None):
         if hasattr(user, 'profile'):
-            return ['uid',]
+            return ['uid', ]
         else:
             return []
 
 
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
-    list_display = ('username', 'uid', 'is_staff',)
+    list_display = ('username', 'uid', 'is_staff', 'is_active', 'email')
 
     def uid(self, user):
         return user.profile.uid if hasattr(user, 'profile') else None
     uid.short_description = 'short unique identifier'
+
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
