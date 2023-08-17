427f275dc80d8c470da450c12123e336e047eac5
Daniele Guido <gui.daniele@gmail.com>
2023-08-17 15:08:00 +0200

    add send_email_password_reset method

--- impresso/utils/tasks/account.py
+++ impresso/utils/tasks/account.py
@@ -10,42 +10,41 @@ default_logger = logging.getLogger(__name__)
 
 
 def getEmailsContents(prefix, context):
-    txt_content = render_to_string(
-        f'emails/{prefix}.txt', context=context)
-    html_content = render_to_string(
-        f'emails/{prefix}.html', context=context)
+    txt_content = render_to_string(f"emails/{prefix}.txt", context=context)
+    html_content = render_to_string(f"emails/{prefix}.html", context=context)
     return txt_content, html_content
 
 
-def send_emails_after_user_registration(
-    user_id, logger=default_logger, test=False
-):
-    logger.info(f'looking for user={user_id}...')
+def send_emails_after_user_registration(user_id, logger=default_logger, test=False):
+    logger.info(f"looking for user={user_id}...")
     try:
         user = User.objects.get(pk=user_id)
     except User.DoesNotExist:
-        logger.info(f'user={user_id} NOT FOUND!')
+        logger.info(f"user={user_id} NOT FOUND!")
         raise
 
-    logger.info(f'user={user_id} active={user.is_active}')
+    logger.info(f"user={user_id} active={user.is_active}")
     view = RegistrationView()
     key = view.get_activation_key(user)
     txt_content, html_content = getEmailsContents(
-        prefix='account_created_mailto_user',
-        context=({
-            'user': user,
-            'key': key
-        })
+        prefix="account_created_mailto_user", context=({"user": user, "key": key})
     )
     try:
         emailMessage = EmailMultiAlternatives(
-            subject='Access to the impresso interface',
+            subject="Access to the impresso interface",
             body=txt_content,
-            from_email=f'Impresso Team <{settings.DEFAULT_FROM_EMAIL}>',
-            to=[user.email, ],
-            cc=[settings.DEFAULT_FROM_EMAIL, ],
-            reply_to=[settings.DEFAULT_FROM_EMAIL, ])
-        emailMessage.attach_alternative(html_content, 'text/html')
+            from_email=f"Impresso Team <{settings.DEFAULT_FROM_EMAIL}>",
+            to=[
+                user.email,
+            ],
+            cc=[
+                settings.DEFAULT_FROM_EMAIL,
+            ],
+            reply_to=[
+                settings.DEFAULT_FROM_EMAIL,
+            ],
+        )
+        emailMessage.attach_alternative(html_content, "text/html")
         emailMessage.send(fail_silently=False)
 
     except Exception:
@@ -55,47 +54,90 @@ def send_emails_after_user_registration(
         print(first_message.subject)
         print(first_message.body)
     except Exception:
-        logger.info(
-            'It looks like the mail settings is not a TEST environment :)')
+        logger.info("It looks like the mail settings is not a TEST environment :)")
         pass
     # send email to the user to confirm the subscription
 
 
-def send_emails_after_user_activation(
-    user_id, logger=default_logger
-):
-    logger.info(f'looking for user={user_id}...')
+def send_emails_after_user_activation(user_id, logger=default_logger):
+    logger.info(f"looking for user={user_id}...")
     try:
         user = User.objects.get(pk=user_id)
     except User.DoesNotExist:
-        logger.info(f'user={user_id} NOT FOUND!')
+        logger.info(f"user={user_id} NOT FOUND!")
         raise
-    logger.info(f'user={user_id} active={user.is_active}')
+    logger.info(f"user={user_id} active={user.is_active}")
     txt_content, html_content = getEmailsContents(
-        prefix='account_activated_mailto_user',
-        context=({
-            'user': user,
-        })
+        prefix="account_activated_mailto_user",
+        context=(
+            {
+                "user": user,
+            }
+        ),
     )
     try:
         emailMessage = EmailMultiAlternatives(
-            subject='Access granted to the impresso interface',
+            subject="Access granted to the impresso interface",
             body=txt_content,
-            from_email=f'Impresso Team <{settings.DEFAULT_FROM_EMAIL}>',
-            to=[user.email, ],
-            cc=[settings.DEFAULT_FROM_EMAIL, ],
-            reply_to=[settings.DEFAULT_FROM_EMAIL, ])
-        emailMessage.attach_alternative(html_content, 'text/html')
+            from_email=f"Impresso Team <{settings.DEFAULT_FROM_EMAIL}>",
+            to=[
+                user.email,
+            ],
+            cc=[
+                settings.DEFAULT_FROM_EMAIL,
+            ],
+            reply_to=[
+                settings.DEFAULT_FROM_EMAIL,
+            ],
+        )
+        emailMessage.attach_alternative(html_content, "text/html")
         emailMessage.send(fail_silently=False)
 
     except Exception:
         raise
     try:
         first_message = mail.outbox[0]
-        logger.info('READING email in THE FAKE OUTBOX:')
+        logger.info("READING email in THE FAKE OUTBOX:")
         print(first_message.subject)
         print(first_message.body)
     except Exception:
-        logger.info(
-            'It looks like the mail settings is not a TEST environment :)')
+        logger.info("It looks like the mail settings is not a TEST environment :)")
         pass
+
+
+def send_email_password_reset(
+    user_id,
+    token="token",
+    callback_url="https://impresso-project.ch/app/reset-password",
+    logger=default_logger,
+):
+    try:
+        user = User.objects.get(pk=user_id, is_active=True)
+    except User.DoesNotExist:
+        logger.info(f"user={user_id} NOT FOUND!")
+        raise
+    txt_content, html_content = getEmailsContents(
+        prefix="account_password_reset",
+        context=(
+            {"user": user, "token": token, "resetLink": f"{callback_url}/{token}"}
+        ),
+    )
+    try:
+        emailMessage = EmailMultiAlternatives(
+            subject="Reset password for impresso",
+            body=txt_content,
+            from_email=f"Impresso Team <{settings.DEFAULT_FROM_EMAIL}>",
+            to=[
+                user.email,
+            ],
+            cc=[
+                settings.DEFAULT_FROM_EMAIL,
+            ],
+            reply_to=[
+                settings.DEFAULT_FROM_EMAIL,
+            ],
+        )
+        emailMessage.attach_alternative(html_content, "text/html")
+        emailMessage.send(fail_silently=False)
+    except Exception:
+        raise
