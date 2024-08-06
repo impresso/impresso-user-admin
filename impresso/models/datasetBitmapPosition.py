a849eca11e82b5a7186727f03757876ac274d385
Daniele Guido <gui.daniele@gmail.com>
2024-08-06 12:20:33 +0200

    Create datasetBitmapPosition.py

---
+++ impresso/models/datasetBitmapPosition.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models import Max
+
+
+class DatasetBitmapPosition(models.Model):
+    name = models.CharField(max_length=255)
+    bitmap_position = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.bitmap_position is None:
+            max_position = DatasetBitmapPosition.objects.aggregate(
+                Max("bitmap_position")
+            )["bitmap_position__max"]
+            self.bitmap_position = 0 if max_position is None else max_position + 1
+        super().save(*args, **kwargs)
