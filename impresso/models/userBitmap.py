e3c792bebfa34b7c5d7a631f66998bb72c2871b7
Daniele Guido <gui.daniele@gmail.com>
2024-08-14 15:33:06 +0200

    Update userBitmap.py

--- impresso/models/userBitmap.py
+++ impresso/models/userBitmap.py
@@ -1,7 +1,10 @@
+import logging
 from django.db import models
 from django.contrib.auth.models import User
 from .datasetBitmapPosition import DatasetBitmapPosition
 from django.db.models.signals import m2m_changed
+
+logger = logging.getLogger(__name__)
 
 
 class UserBitmap(models.Model):
@@ -36,21 +39,27 @@ class UserBitmap(models.Model):
             bitmap = UserBitmap.USER_PLAN_EDUCATIONAL
         else:
             bitmap = UserBitmap.USER_PLAN_AUTH_USER
+        # print current bitmap
+        # print(f"current bitmap: {bitmap:05b}")
         # get all user subscriptions
         subscriptions = list(self.subscriptions.values("name", "bitmap_position"))
+        if not subscriptions:
+            return bitmap
         # max bitmap position
         max_position = (
             max([x["bitmap_position"] for x in subscriptions])
             + UserBitmap.BITMAP_PLAN_MAX_LENGTH
+            + 1
         )
         # Shift the initial signature to the left by the max bit position
         bitmap = bitmap << max_position - UserBitmap.BITMAP_PLAN_MAX_LENGTH
+        # print(f"current empty bitmap: {bitmap:05b}")
         for subscription in subscriptions:
             # Use the bitmap position to set the corresponding bit
             position = (
                 subscription["bitmap_position"] + UserBitmap.BITMAP_PLAN_MAX_LENGTH
             )
-            bitmap |= 1 << (max_position - position)
+            bitmap |= 1 << (max_position - position - 1)
 
         return bitmap
 
@@ -63,16 +72,38 @@ class UserBitmap(models.Model):
 
 
 def update_user_bitmap(sender, instance, action, **kwargs):
-    if action == "post_add":
+    if action == "post_add" or action == "post_remove":
+        logger.info(f"User {instance.user} subscription changed, updating")
         user_bitmap = instance.get_up_to_date_bitmap()
         bitmap_bytes = user_bitmap.to_bytes(
             (user_bitmap.bit_length() + 7) // 8, byteorder="big"
         )
         instance.bitmap = bitmap_bytes
         instance.save()
+        logger.info(
+            f"User {instance.user} subscription changed, bitmap updated to {user_bitmap:05b}"
+        )
+
+
+def update_user_bitmap_on_user_groups_changed(sender, instance, action, **kwargs):
+    if action == "post_add" or action == "post_remove":
+        user_bitmap, created = UserBitmap.objects.get_or_create(user=instance)
+        logger.info(
+            f"User {instance} groups changed. {'Creating new bitmap.' if created else 'Updating bitmap.'}"
+        )
+        bitmap = user_bitmap.get_up_to_date_bitmap()
+        bitmap_bytes = bitmap.to_bytes((bitmap.bit_length() + 7) // 8, byteorder="big")
+        user_bitmap.bitmap = bitmap_bytes
+        user_bitmap.save()
+        logger.info(f"User {instance} groups changed, bitmap updated to {bitmap:05b}")
 
 
 m2m_changed.connect(
     update_user_bitmap,
     sender=UserBitmap.subscriptions.through,
+)
+
+m2m_changed.connect(
+    update_user_bitmap_on_user_groups_changed,
+    sender=User.groups.through,
 )
