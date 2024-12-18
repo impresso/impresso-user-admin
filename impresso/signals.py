131c1f8feb5558392f4e6605e12fd5cec7303a44
Daniele Guido <1181642+danieleguido@users.noreply.github.com>
2024-12-18 17:09:17 +0100

    Create signals.py

---
+++ impresso/signals.py
@@ -0,0 +1,44 @@
+import logging
+from django.conf import settings
+from django.contrib.auth.models import Group
+
+logger = logging.getLogger(__name__)
+
+
+def create_default_groups(sender, **kwargs):
+    """
+    Creates default user groups based on the settings.
+
+    This function is typically used as a signal handler to create default
+    user groups when certain events occur (e.g., when a user is created).
+
+    Args:
+      sender (Any): The sender of the signal.
+      **kwargs: Additional keyword arguments passed by the signal.
+
+    Settings:
+      IMPRESSO_DEFAULT_GROUP_USERS (list of tuple): A list of tuples where each
+      tuple contains the slug and the name of the group to be created.
+
+    Logs:
+      Logs the creation of default groups using the logger.
+
+    Example:
+      default_groups = [
+        ('admin', 'Administrators'),
+        ('editor', 'Editors'),
+        ('viewer', 'Viewers')
+      ]
+      settings.IMPRESSO_DEFAULT_GROUP_USERS = default_groups
+      create_default_groups(sender=None)
+    """
+    default_groups = settings.IMPRESSO_DEFAULT_GROUP_USERS
+
+    logger.info(f"Creating default groups: {[a[0] for a in default_groups]}")
+    # each item is a tuple, containing the slud and the name of the group
+    for group_slug, group_name in default_groups:
+        g, created = Group.objects.get_or_create(name=group_slug)
+        if created:
+            logger.info(f"Group created successfully: {group_slug}")
+        else:
+            logger.info(f"Group already exists: {g.id} - {group_slug}")
