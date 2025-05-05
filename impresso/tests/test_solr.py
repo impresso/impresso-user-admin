ef907f472ece31fc60ecce75a5d679a52e8f38c6
Daniele Guido <gui.daniele@gmail.com>
2025-05-05 16:57:17 +0200

    Update test_solr.py

--- impresso/tests/test_solr.py
+++ impresso/tests/test_solr.py
@@ -3,16 +3,21 @@ from django.conf import settings
 from impresso.utils.solr import serialize_solr_doc_content_item_to_plain_dict
 from impresso.utils.solr import mapper_doc_redact_contents
 from impresso.utils.bitmask import BitMask64
+from typing import Any, Dict
 
 
 class SolrTestCase(unittest.TestCase):
+    """
+    run ./manage.py test impresso.tests.test_solr.SolrTestCase
+    """
+
     # def test_JsonWithBitmapDecoder(self):
     #     # Test the JsonWithBitmapDecoder with a valid input
     #     original = '{"bm_get_tr_bin": 0b1010}'
     #     result = json.loads(original, cls=JsonWithBitmapDecoder)
 
     #     self.assertEqual(result, {"bm_get_tr_bin": "0101"})
-    def test_serialize_solr_doc_content_item_to_plain_dict(self):
+    def test_serialize_solr_doc_content_item_to_plain_dict(self) -> None:
         # Test the function with a valid input, a document parsed from solr
         result = serialize_solr_doc_content_item_to_plain_dict(FAKE_SOLR_DOC)
 
@@ -46,7 +51,7 @@ class SolrTestCase(unittest.TestCase):
                 "bm_get_tr_i": 181,
             }
         )
-
+        print(doc)
         # Test the function with a valid input, a document parsed from solr
         result_redacted = mapper_doc_redact_contents(
             doc={**doc},
@@ -71,7 +76,7 @@ class SolrTestCase(unittest.TestCase):
         )
 
 
-FAKE_SOLR_DOC = {
+FAKE_SOLR_DOC: Dict[str, Any] = {
     "id": "johndoe-1927-11-15-a-i0009",
     "item_type_s": "ar",
     "lg_s": "de",
@@ -99,14 +104,12 @@ FAKE_SOLR_DOC = {
     "meta_date_dt": "1927-11-15T00:00:00Z",
     "pers_mentions": None,
     "loc_mentions": None,
-    "access_right_s": "OpenPublic",
     "score": 1.0,
     "exportable_plain": None,
     "topics_dpfs": [
         "tm-de-all-v2.0_tp01_de|0.02 tm-de-all-v2.0_tp03_de|0.166 tm-de-all-v2.0_tp11_de|0.026 "
     ],
     "ucoll_ss": None,
-    "bm_get_tr_s": None,
-    "bm_get_tr_bin": None,
-    "bm_get_tr_i": 181,
+    settings.IMPRESSO_SOLR_FL_COPYRIGHT: "in_cpy",
+    settings.IMPRESSO_SOLR_FL_TRANSCRIPT_BM: 9223372036854775807,
 }
