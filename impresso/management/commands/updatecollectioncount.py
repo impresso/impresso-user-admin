2fbd260cc01f48f1c3e78dbe8aa48cc07e152a8b
Daniele Guido <gui.daniele@gmail.com>
2025-07-03 16:53:57 +0200

    improve comand to check and correct collections

--- impresso/management/commands/updatecollectioncount.py
+++ impresso/management/commands/updatecollectioncount.py
@@ -6,23 +6,33 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = 'update count for collected items in a collection'
+    help = "update count for collected items in a collection"
 
     def add_arguments(self, parser):
-        parser.add_argument('collection_id', nargs='+', type=str)
+        parser.add_argument("collection_ids", nargs="+", type=str)
 
-    def handle(self, *args, **options):
-        self.stdout.write('sync: %s' % options['collection_id'])
-        collections = Collection.objects.filter(pk__in=options['collection_id'])
-        self.stdout.write('n. collection to sync: %s' % collections.count())
+    def handle(self, collection_ids, *args, **options):
+        self.stdout.write(self.style.HTTP_INFO(f"\n\nUpdate collection count_item"))
+        self.stdout.write(
+            f" - collection_idss={collection_ids}",
+        )
+        self.stdout.write("sync: %s" % collection_ids)
+        collections = Collection.objects.filter(pk__in=collection_ids)
+        self.stdout.write("n. collection to sync: %s" % collections.count())
 
         for collection in collections:
-            self.stdout.write('start updating collection "{}"(pk={})...'.format(collection.name, collection.pk))
+            self.stdout.write(
+                'start updating collection "{}"(pk={})...'.format(
+                    collection.name, collection.pk
+                )
+            )
             count_in_solr = collection.update_count_items()
-            count_in_db = CollectableItem.objects.filter(collection = collection).count()
-            self.stdout.write('done, collection "{}"(pk={}) updated, count_items: {}, in db: {}'.format(
-                collection.name,
-                collection.pk,
-                count_in_solr,
-                count_in_db,
-            ))
+            count_in_db = CollectableItem.objects.filter(collection=collection).count()
+            self.stdout.write(
+                'done, collection "{}"(pk={}) updated, count_items: {}, in db: {}'.format(
+                    collection.name,
+                    collection.pk,
+                    count_in_solr,
+                    count_in_db,
+                )
+            )
